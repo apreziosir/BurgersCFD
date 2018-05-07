eb93aad3577d87d912042d237139417cb6d0ce66
apreziosir <toni@Debian2>
2018-05-06 22:22:56 -0500

    diffusive term matrix assembly started

--- Burgers1D.py
+++ Burgers1D.py
@@ -8,6 +8,7 @@ May 2018
 """
 
 import numpy as np
+import scipy.sparse as sp
 import matplotlib.pyplot as plt
 from matplotlib import style
 import Analytical as an
@@ -22,7 +23,7 @@ X0 = -1
 XF = 1
 nu = 0.1
 t0 = 0
-tf = 
+tf = 3
 
 # ==============================================================================
 # Numerical parameters declaration
@@ -31,11 +32,28 @@ tf =
 # Number of nodes in the domain (changeable for convergence analysis)
 N = 101
 
+# Number of timesteps for the calculation
+nT = 2000
+
 # Calculating dx for value between nodes
 dx = (XF - X0) / (N - 1)
 
 # Generating vector of nodes
 xn = np.linspace(X0, XF, N)
+
+# Generating vector of times
+ts = np.linspace(t0, tf, nT) / np.pi 
+
+# Calculating timestep size from timestep vector
+dt = ts[1] - ts[0]
+
+# ==============================================================================
+# Assembling sparse matrix for viscous term solution. Efficient storage of 
+# matrix for memory purposes
+# ==============================================================================
+
+# Defining matrix
+K = sp.lil_matrix((N, N))
 
 
 
@@ -57,3 +75,17 @@ plt.xlabel(r'Distance $(m)$')
 plt.ylabel(r'Velocity $ \frac{m}{s} $')
 plt.draw()
 plt.pause(1.5)
+
+# ==============================================================================
+# Startting time loop of the program (solved via fractional steps)
+# ==============================================================================
+
+for t in range(1, len(ts)):
+    
+    # Calculating the non linear term using one of the three possibilities
+    
+    
+    # Calculating the diffusive term with high order derivatives in the internal
+    # nodes (5 points approximation)
+    
+    
